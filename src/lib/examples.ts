fd4aa56893aadc85ca7e3f8e9c41acfe6f6a6568
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2026-04-28 10:38:14 +0300

    added example with function call

--- src/lib/examples.ts
+++ src/lib/examples.ts
@@ -2,6 +2,11 @@ import type { LanguageName } from "./parser";
 
 export const EXAMPLES: Array<Record<LanguageName, string>> = [
   {
+    JavaScript: `function main() {\n  print("Hello, World!");\n}\n`,
+    Python: `def main():\n    print("Hello, World!")\n`,
+    Lua: `function main()\n  print("Hello, World!")\nend\n`,
+  },
+  {
     JavaScript: `function main(argc, argv) {\n  let abc = 1;\n  return abc;\n}\n`,
     Python: `def main(argc, argv):\n    abc = 1\n    return abc\n`,
     Lua: `function main(argc, argv)\n  local abc = 1\n  return abc\nend\n`,
